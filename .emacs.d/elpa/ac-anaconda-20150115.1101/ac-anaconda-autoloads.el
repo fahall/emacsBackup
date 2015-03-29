423ad05edb23469628db836d7889c51a49974f2c
AlexHall <alexhall@eecs.berkeley.edu>
2015-03-29 14:22:51 -0700

    uploaded most recent mac settings

---
+++ .emacs.d/elpa/ac-anaconda-20150115.1101/ac-anaconda-autoloads.el
@@ -0,0 +1,29 @@
+;;; ac-anaconda-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ac-anaconda-setup) "ac-anaconda" "ac-anaconda.el"
+;;;;;;  (21734 60160 0 0))
+;;; Generated autoloads from ac-anaconda.el
+
+(autoload 'ac-anaconda-setup "ac-anaconda" "\
+Set up `ac-sources' for `anaconda-mode'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-anaconda-pkg.el") (21734 60160 527001
+;;;;;;  0))
+
+;;;***
+
+(provide 'ac-anaconda-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-anaconda-autoloads.el ends here
