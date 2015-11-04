ab9395cdf531291850221ca3e506756f760d47c3
FAHall <fahall1221@gmail.com>
2015-11-04 13:51:42 -0800

    Automated Commit

--- init.el
+++ init.el
@@ -117,6 +117,8 @@
  (add-to-list 'load-path "~/emacsBackup/.emacs.d/syntax-modes/")
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
