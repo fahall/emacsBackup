5e0f574a57bbed93f53468d8a5e9da79311b0949
Alex Hall <alexhall@eecs.berkeley.edu>
2016-10-15 14:15:07 -0700

    Automated Commit

--- init.el
+++ init.el
@@ -349,28 +349,3 @@
   (set-window-width 80))
 
 (global-set-key "\C-x~" 'set-80-columns)
-
-(defun fix-frame-horizontal-size (width)
-  "Set the frame's size to 80 (or prefix arg WIDTH) columns wide."
-  (interactive "P")
-  (if window-system
-      (set-frame-width (selected-frame) (or width 80))
-    (error "Cannot resize frame horizontally: is a text terminal")))
-
-(defun fix-window-horizontal-size (width)
-  "Set the window's size to 80 (or prefix arg WIDTH) columns wide."
-  (interactive "P")
-  (enlarge-window (- (or width 80) (window-width)) 'horizontal))
-
-(defun fix-horizontal-size (width)
-  "Set the window's or frame's width to 80 (or prefix arg WIDTH)."
-  (interactive "P")
-  (condition-case nil
-      (fix-window-horizontal-size width)
-    (error 
-     (condition-case nil
-	 (fix-frame-horizontal-size width)
-       (error
-	(error "Cannot resize window or frame horizontally"))))))
-
-(global-set-key (kbd "C-x W") 'fix-horizontal-size)
