d13ebcd0fb5c93f027a17514eb93ece47af3d395
Alex Hall <alexhall@eecs.berkeley.edu>
2017-03-10 14:00:43 -0800

    Automated Commit

--- init.el
+++ init.el
@@ -313,70 +313,98 @@
 (global-set-key "\C-x~" 'set-80-columns)
 
 
-
+(use-package ivy :ensure t
+  :diminish (ivy-mode . "")
+  :bind
+  (:map ivy-mode-map
+   ("C-'" . ivy-avy))
+  :config
+  (ivy-mode 1)
+  ;; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’.
+  (setq ivy-use-virtual-buffers t)
+  ;; number of result lines to display
+  (setq ivy-height 10)
+  ;; does not count candidates
+  (setq ivy-count-format "")
+  ;; no regexp by default
+  (setq ivy-initial-inputs-alist nil)
+  ;; configure regexp engine.
+  (setq ivy-re-builders-alist
+	;; allow input not in order
+        '((t   . ivy--regex-ignore-order))))
 ;; Projectile Mode
 (setq projectile-mode t)
 
-;; HELM setup
-(require 'helm)
-(setq helm-ff-transformer-show-only-basename nil
-      helm-adaptive-history-file             "~/emacsBackup/.emacs.d/data/helm-history"
-      helm-yank-symbol-first                 t
-      helm-move-to-line-cycle-in-source      t
-      helm-buffers-fuzzy-matching            t
-      helm-ff-auto-update-initial-value      t)
-
-(autoload 'helm-descbinds      "helm-descbinds" t)
-(autoload 'helm-eshell-history "helm-eshell"    t)
-(autoload 'helm-esh-pcomplete  "helm-eshell"    t)
-(helm-autoresize-mode t)
-(global-set-key (kbd "C-h a")    #'helm-apropos)
-(global-set-key (kbd "C-h i")    #'helm-info-emacs)
-(global-set-key (kbd "C-h b")    #'helm-descbinds)
-
-(add-hook 'eshell-mode-hook
-          #'(lambda ()
-              (define-key eshell-mode-map (kbd "TAB")     #'helm-esh-pcomplete)
-              (define-key eshell-mode-map (kbd "C-c C-l") #'helm-eshell-history)))
-
-(global-set-key (kbd "C-x b")   #'helm-mini)
-(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
-(global-set-key (kbd "C-x C-m") #'helm-M-x)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(global-set-key (kbd "C-x C-r") #'helm-recentf)
-(global-set-key (kbd "C-x r l") #'helm-filtered-bookmarks)
-(global-set-key (kbd "M-y")     #'helm-show-kill-ring)
-(global-set-key (kbd "M-s o")   #'helm-swoop)
-(global-set-key (kbd "M-s /")   #'helm-multi-swoop)
-
-(require 'helm-config)
-(helm-mode t)
-(helm-adaptive-mode t)
-
-(global-set-key (kbd "C-x c!")   #'helm-calcul-expression)
-(global-set-key (kbd "C-x c:")   #'helm-eval-expression-with-eldoc)
-(define-key helm-map (kbd "M-o") #'helm-previous-source)
-
-(global-set-key (kbd "M-s s")   #'helm-ag)
-
-(require 'helm-projectile)
-(setq helm-projectile-sources-list (cons 'helm-source-projectile-files-list
-                                         (remove 'helm-source-projectile-files-list 
-                                              helm-projectile-sources-list)))
-(helm-projectile-on)
-
-(define-key projectile-mode-map (kbd "C-c p /")
-  #'(lambda ()
-      (interactive)
-      (helm-ag (projectile-project-root))))
-
-(add-hook 'org-mode-hook
-          (lambda ()
-          (define-key org-mode-map (kbd "C-x c o h") #'helm-org-headlines)))
-;; End helm setup
-
-
-;; Adapted From Better Defaults.el
+;;;; HELM setup
+;;(require 'helm)
+;;(setq helm-ff-transformer-show-only-basename nil
+;;      helm-adaptive-history-file             "~/emacsBackup/.emacs.d/data/helm-history"
+;;      helm-yank-symbol-first                 t
+;;      helm-move-to-line-cycle-in-source      t
+;;      helm-buffers-fuzzy-matching            t
+;;      helm-ff-auto-update-initial-value      t)
+;;
+;;(autoload 'helm-descbinds      "helm-descbinds" t)
+;;(autoload 'helm-eshell-history "helm-eshell"    t)
+;;(autoload 'helm-esh-pcomplete  "helm-eshell"    t)
+;;(helm-autoresize-mode t)
+;;(global-set-key (kbd "C-h a")    #'helm-apropos)
+;;(global-set-key (kbd "C-h i")    #'helm-info-emacs)
+;;(global-set-key (kbd "C-h b")    #'helm-descbinds)
+;;
+;;(add-hook 'eshell-mode-hook
+;;          #'(lambda ()
+;;              (define-key eshell-mode-map (kbd "TAB")     #'helm-esh-pcomplete)
+;;              (define-key eshell-mode-map (kbd "C-c C-l") #'helm-eshell-history)))
+;;
+;;(global-set-key (kbd "C-x b")   #'helm-mini)
+;;(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+;;(global-set-key (kbd "C-x C-m") #'helm-M-x)
+;;(global-set-key (kbd "C-x C-f") #'helm-find-files)
+;;(global-set-key (kbd "C-x C-r") #'helm-recentf)
+;;(global-set-key (kbd "C-x r l") #'helm-filtered-bookmarks)
+;;(global-set-key (kbd "M-y")     #'helm-show-kill-ring)
+;;(global-set-key (kbd "M-s o")   #'helm-swoop)
+;;(global-set-key (kbd "M-s /")   #'helm-multi-swoop)
+;;
+;;(require 'helm-config)
+;;(helm-mode t)
+;;(helm-adaptive-mode t)
+;;
+;;(global-set-key (kbd "C-x c!")   #'helm-calcul-expression)
+;;(global-set-key (kbd "C-x c:")   #'helm-eval-expression-with-eldoc)
+;;(define-key helm-map (kbd "M-o") #'helm-previous-source)
+;;
+;;(global-set-key (kbd "M-s s")   #'helm-ag)
+;;
+;;(require 'helm-projectile)
+;;(setq helm-projectile-sources-list (cons 'helm-source-projectile-files-list
+;;                                         (remove 'helm-source-projectile-files-list 
+;;                                              helm-projectile-sources-list)))
+;;(helm-projectile-on)
+;;
+;;(define-key projectile-mode-map (kbd "C-c p /")
+;;  #'(lambda ()
+;;      (interactive)
+;;      (helm-ag (projectile-project-root))))
+;;
+;;(add-hook 'org-mode-hook
+;;          (lambda ()
+;;            (define-key org-mode-map (kbd "C-x c o h") #'helm-org-headlines)))
+;;
+;;(require 'helm-flyspell)
+;;;; add flyspell correction to helm
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(define-key flyspell-mode-map (kbd "C-;") 'helm-flyspell-correct)
+;;(define-key flycheck-mode-map (kbd "C-;") 'helm-flycheck)
+;;
+;;(global-set-key "\C-cC-r" 'helm-org-rifle-org-directory)
+;;(global-set-key "\C-cr" 'helm-org-rifle)
+;;
+;;;; End helm setup
+;;
+;;
+;;;; Adapted From Better Defaults.el
 (unless (fboundp 'helm-mode)
     (ido-mode t)
     (setq ido-enable-flex-matching t))
@@ -625,17 +653,9 @@ of change will be 23:59 on that day"
   (rainbow-mode 1)
   )
 
-(global-set-key "\C-cC-r" 'helm-org-rifle-org-directory)
-(global-set-key "\C-cr" 'helm-org-rifle)
 
 (add-to-list 'org-tag-alist '("TOC" . ?T))
 (require 'wc-mode)
-
-(require 'helm-flyspell)
-;; add flyspell correction to helm
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(define-key flyspell-mode-map (kbd "C-;") 'helm-flyspell-correct)
-(define-key flycheck-mode-map (kbd "C-;") 'helm-flycheck)
 
 (provide 'init)
 
