5d4d8afa4e5b26c2ca805d778b1473357e48a74a
Alex Hall <alexhall@eecs.berkeley.edu>
2017-03-10 14:21:05 -0800

    Automated Commit

--- init.el
+++ init.el
@@ -645,5 +645,26 @@ of change will be 23:59 on that day"
 (add-to-list 'org-tag-alist '("TOC" . ?T))
 (require 'wc-mode)
 
-(provide 'init)
 
+
+(setq helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+
+      helm-echo-input-in-header-line t) ;; input close to where I type
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+(add-hook 'helm-minibuffer-set-up-hook
+      'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+(provide 'init)
