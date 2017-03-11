848d8e2c9babad5cd5d5eeee52bd440571fb3747
Alex Hall <alexhall@eecs.berkeley.edu>
2017-03-11 14:54:33 -0800

    Automated Commit

--- init.el
+++ init.el
@@ -9,16 +9,5 @@
 (org-dotemacs-load-default)
 
 
-;;(load "my_basic_settings.elc" )
-;;(load "my_better_defaults.elc")
-;;(load "my_window_management.elc")
-;;(load "my_syntax_settings.elc")
-;;(load "my_auto_complete_settings.elc" )
-;;(load "my_project_management.elc" )
-;;(load "my_helm.elc")
-;;(load "my_org_settings.elc" )
-;;(load "my_version_control.elc" )
-;;(load "~/emacsBackup/my_ivy.elc") ;; this is a helm replacement candidate
-
 (provide 'init)
 ;;; init.el ends here
