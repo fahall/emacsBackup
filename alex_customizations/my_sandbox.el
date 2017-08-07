72fe4bc3d97f3d71b813b9a99ded1840b6bb881f
FAHall <fahall1221@gmail.com>
2017-08-07 14:13:37 -0700

    temporarily disabling godot stuff

--- alex_customizations/my_sandbox.el
+++ alex_customizations/my_sandbox.el
@@ -1,11 +1,11 @@
 
-;; Godot GDScript settings
-(add-to-list 'load-path "~/emacsBackup/.emacs.d/godot-gdscript.el/")
-(require 'godot-gdscript)
-(require 'company)
-(require 'company-godot-gdscript)
-(eval-after-load "company"
-  '(progn
-     (add-to-list 'company-backends 'company-godot-gdscript)))
-(add-hook 'godot-gdscript-mode-hook 'company-mode)
-;; END Godot GDScript settings
+;;;; Godot GDScript settings
+;;(add-to-list 'load-path "~/emacsBackup/.emacs.d/godot-gdscript.el/")
+;;(autoload 'godot-gdscript)
+;;(require 'company)
+;;(autoload 'company-godot-gdscript)
+;;(eval-after-load "company"
+;;  '(progn
+;;     (add-to-list 'company-backends 'company-godot-gdscript)))
+;;(add-hook 'godot-gdscript-mode-hook 'company-mode)
+;;;; END Godot GDScript settings
--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 ;;; Commentary: This master file calls subcomponents in the alex_customizations directory
 ;;; Code:
 (add-to-list 'load-path "~/emacsBackup/alex_customizations/")
+(add-to-list 'load-path "~/emacsBackup/.emacs.d/")
 (load "my_package_management.el" ) ;; MUST LOAD FIRST
 (load "my_basics")
 (load "my_window_management")
@@ -18,3 +19,34 @@
 
 (provide 'init)
 ;;; init.el ends here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
